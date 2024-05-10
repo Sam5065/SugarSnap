c5680a46e1b70de9cc721c148be967048cca1098
Sam5065 <sam.hilland1@icloud.com>
2024-05-10 18:50:14 +0100

    Added Jade item

--- README.md
+++
@@ -1,7 +0,0 @@
-# SugarSnap
-
-This mod adds back the paper recipe from the base game, in my server other mods overrided the recipe so this mod adds it back.
-
-Created using MCreator for forge 1.16.5
-https://mcreator.net/
-
---
+++ src/main/java/net/mcreator/sugarsnap/item/JadeitemItem.java
@@ -0,0 +1,55 @@
+
+package net.mcreator.sugarsnap.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.sugarsnap.SugarsnapModElements;
+
+@SugarsnapModElements.ModElement.Tag
+public class JadeitemItem extends SugarsnapModElements.ModElement {
+	@ObjectHolder("sugarsnap:jadeitem")
+	public static final Item block = null;
+	public JadeitemItem(SugarsnapModElements instance) {
+		super(instance, 3);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MATERIALS).maxStackSize(64).rarity(Rarity.COMMON));
+			setRegistryName("jadeitem");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+
+		@Override
+		@OnlyIn(Dist.CLIENT)
+		public boolean hasEffect(ItemStack itemstack) {
+			return true;
+		}
+	}
+}
