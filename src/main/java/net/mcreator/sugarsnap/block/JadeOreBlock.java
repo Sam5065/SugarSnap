4fee6bd1deb12207d88a8a47e68351812818f620
Sam5065 <sam.hilland1@icloud.com>
2024-05-11 12:33:29 +0100

    Decreased spawn rate

--- src/main/java/net/mcreator/sugarsnap/block/JadeOreBlock.java
+++ src/main/java/net/mcreator/sugarsnap/block/JadeOreBlock.java
@@ -113,7 +113,7 @@ public class JadeOreBlock extends SugarsnapModElements.ModElement {
 					return super.generate(world, generator, rand, pos, config);
 				}
 			};
-			configuredFeature = feature.withConfiguration(new OreFeatureConfig(CustomRuleTest.INSTANCE, block.getDefaultState(), 16)).range(100)
+			configuredFeature = feature.withConfiguration(new OreFeatureConfig(CustomRuleTest.INSTANCE, block.getDefaultState(), 3)).range(100)
 					.square().func_242731_b(10);
 			event.getRegistry().register(feature.setRegistryName("jade_ore"));
 			Registry.register(WorldGenRegistries.CONFIGURED_FEATURE, new ResourceLocation("sugarsnap:jade_ore"), configuredFeature);
