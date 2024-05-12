49fb40abe89796e8e6cbeb4daf0049ccbd0eed72
Sam5065 <sam.hilland1@icloud.com>
2024-05-12 18:07:43 +0100

    Added axe

---
+++ src/main/java/net/mcreator/sugarsnap/item/JadeAxeItem.java
@@ -0,0 +1,52 @@
+
+package net.mcreator.sugarsnap.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.AxeItem;
+
+import net.mcreator.sugarsnap.SugarsnapModElements;
+
+@SugarsnapModElements.ModElement.Tag
+public class JadeAxeItem extends SugarsnapModElements.ModElement {
+	@ObjectHolder("sugarsnap:jade_axe")
+	public static final Item block = null;
+
+	public JadeAxeItem(SugarsnapModElements instance) {
+		super(instance, 19);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new AxeItem(new IItemTier() {
+			public int getMaxUses() {
+				return 100;
+			}
+
+			public float getEfficiency() {
+				return 12f;
+			}
+
+			public float getAttackDamage() {
+				return 2f;
+			}
+
+			public int getHarvestLevel() {
+				return 2;
+			}
+
+			public int getEnchantability() {
+				return 2;
+			}
+
+			public Ingredient getRepairMaterial() {
+				return Ingredient.EMPTY;
+			}
+		}, 1, -3f, new Item.Properties().group(ItemGroup.TOOLS)) {
+		}.setRegistryName("jade_axe"));
+	}
+}
