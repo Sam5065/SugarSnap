e3c9400417121006142efd612a86e87b48068e20
Sam5065 <sam.hilland1@icloud.com>
2024-05-12 18:08:38 +0100

    Added sword

---
+++ src/main/java/net/mcreator/sugarsnap/item/JadeSwordItem.java
@@ -0,0 +1,52 @@
+
+package net.mcreator.sugarsnap.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+
+import net.mcreator.sugarsnap.SugarsnapModElements;
+
+@SugarsnapModElements.ModElement.Tag
+public class JadeSwordItem extends SugarsnapModElements.ModElement {
+	@ObjectHolder("sugarsnap:jade_sword")
+	public static final Item block = null;
+
+	public JadeSwordItem(SugarsnapModElements instance) {
+		super(instance, 20);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new SwordItem(new IItemTier() {
+			public int getMaxUses() {
+				return 100;
+			}
+
+			public float getEfficiency() {
+				return 4f;
+			}
+
+			public float getAttackDamage() {
+				return 2f;
+			}
+
+			public int getHarvestLevel() {
+				return 2;
+			}
+
+			public int getEnchantability() {
+				return 2;
+			}
+
+			public Ingredient getRepairMaterial() {
+				return Ingredient.EMPTY;
+			}
+		}, 3, -2f, new Item.Properties().group(ItemGroup.TOOLS)) {
+		}.setRegistryName("jade_sword"));
+	}
+}
