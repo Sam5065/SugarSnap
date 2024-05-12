47b4d65f661e82c89e66c5d6b50b456fb52eb854
Sam5065 <sam.hilland1@icloud.com>
2024-05-12 21:15:12 +0100

    Created new jade block

---
+++ src/main/java/net/mcreator/sugarsnap/block/FireiumJadeBlockBlock.java
@@ -0,0 +1,60 @@
+
+package net.mcreator.sugarsnap.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.common.ToolType;
+
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.sugarsnap.SugarsnapModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@SugarsnapModElements.ModElement.Tag
+public class FireiumJadeBlockBlock extends SugarsnapModElements.ModElement {
+	@ObjectHolder("sugarsnap:fireium_jade_block")
+	public static final Block block = null;
+
+	public FireiumJadeBlockBlock(SugarsnapModElements instance) {
+		super(instance, 30);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
+	}
+
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.METAL).hardnessAndResistance(1f, 10f).setLightLevel(s -> 0).harvestLevel(2)
+					.harvestTool(ToolType.PICKAXE).setRequiresTool());
+			setRegistryName("fireium_jade_block");
+		}
+
+		@Override
+		public int getOpacity(BlockState state, IBlockReader worldIn, BlockPos pos) {
+			return 15;
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
