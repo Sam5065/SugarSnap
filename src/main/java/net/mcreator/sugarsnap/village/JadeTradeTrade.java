ebe0cd3db6f185a835c75d9cfd1b33d60d6fea31
Sam5065 <sam.hilland1@icloud.com>
2024-05-12 19:40:36 +0100

    Added trade

---
+++ src/main/java/net/mcreator/sugarsnap/village/JadeTradeTrade.java
@@ -0,0 +1,29 @@
+
+package net.mcreator.sugarsnap.village;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.village.VillagerTradesEvent;
+import net.minecraftforge.common.BasicTrade;
+
+import net.minecraft.item.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.entity.merchant.villager.VillagerTrades;
+import net.minecraft.entity.merchant.villager.VillagerProfession;
+
+import net.mcreator.sugarsnap.item.JadeitemItem;
+
+import java.util.List;
+
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class JadeTradeTrade {
+	@SubscribeEvent
+	public static void registerTrades(VillagerTradesEvent event) {
+		Int2ObjectMap<List<VillagerTrades.ITrade>> trades = event.getTrades();
+		if (event.getType() == VillagerProfession.CLERIC) {
+			trades.get(5).add(new BasicTrade(new ItemStack(Items.GOLD_INGOT, (int) (10)), new ItemStack(JadeitemItem.block), 10, 5, 0.05f));
+		}
+	}
+}
