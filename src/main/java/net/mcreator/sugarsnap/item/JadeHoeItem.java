969f45bce986ff2cc6ed004379e04c3e8011d165
Sam5065 <sam.hilland1@icloud.com>
2024-05-12 18:14:11 +0100

    Added recipes

---
+++ src/main/java/net/mcreator/sugarsnap/item/JadeHoeItem.java
@@ -0,0 +1,53 @@
+
+package net.mcreator.sugarsnap.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.HoeItem;
+
+import net.mcreator.sugarsnap.SugarsnapModElements;
+
+@SugarsnapModElements.ModElement.Tag
+public class JadeHoeItem extends SugarsnapModElements.ModElement {
+	@ObjectHolder("sugarsnap:jade_hoe")
+	public static final Item block = null;
+
+	public JadeHoeItem(SugarsnapModElements instance) {
+		super(instance, 25);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new HoeItem(new IItemTier() {
+			public int getMaxUses() {
+				return 100;
+			}
+
+			public float getEfficiency() {
+				return 10f;
+			}
+
+			public float getAttackDamage() {
+				return 2f;
+			}
+
+			public int getHarvestLevel() {
+				return 1;
+			}
+
+			public int getEnchantability() {
+				return 2;
+			}
+
+			public Ingredient getRepairMaterial() {
+				return Ingredient.fromStacks(new ItemStack(RawJadeItemItem.block));
+			}
+		}, 0, -3f, new Item.Properties().group(ItemGroup.TOOLS)) {
+		}.setRegistryName("jade_hoe"));
+	}
+}
