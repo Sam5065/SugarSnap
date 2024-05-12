db0c16aeadee49351a03df3963f38b988990ce18
Sam5065 <sam.hilland1@icloud.com>
2024-05-12 01:22:59 +0100

    Added armour

---
+++ src/main/java/net/mcreator/sugarsnap/item/JadeArmorItem.java
@@ -0,0 +1,106 @@
+
+package net.mcreator.sugarsnap.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.sugarsnap.SugarsnapModElements;
+
+@SugarsnapModElements.ModElement.Tag
+public class JadeArmorItem extends SugarsnapModElements.ModElement {
+	@ObjectHolder("sugarsnap:jade_armor_helmet")
+	public static final Item helmet = null;
+	@ObjectHolder("sugarsnap:jade_armor_chestplate")
+	public static final Item body = null;
+	@ObjectHolder("sugarsnap:jade_armor_leggings")
+	public static final Item legs = null;
+	@ObjectHolder("sugarsnap:jade_armor_boots")
+	public static final Item boots = null;
+
+	public JadeArmorItem(SugarsnapModElements instance) {
+		super(instance, 16);
+	}
+
+	@Override
+	public void initElements() {
+		IArmorMaterial armormaterial = new IArmorMaterial() {
+			@Override
+			public int getDurability(EquipmentSlotType slot) {
+				return new int[]{13, 15, 16, 11}[slot.getIndex()] * 25;
+			}
+
+			@Override
+			public int getDamageReductionAmount(EquipmentSlotType slot) {
+				return new int[]{2, 5, 6, 2}[slot.getIndex()];
+			}
+
+			@Override
+			public int getEnchantability() {
+				return 12;
+			}
+
+			@Override
+			public net.minecraft.util.SoundEvent getSoundEvent() {
+				return (net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("item.armor.equip_chain"));
+			}
+
+			@Override
+			public Ingredient getRepairMaterial() {
+				return Ingredient.fromStacks(new ItemStack(JadeitemItem.block), new ItemStack(RawJadeItemItem.block));
+			}
+
+			@OnlyIn(Dist.CLIENT)
+			@Override
+			public String getName() {
+				return "jade_armor";
+			}
+
+			@Override
+			public float getToughness() {
+				return 0f;
+			}
+
+			@Override
+			public float getKnockbackResistance() {
+				return 0f;
+			}
+		};
+		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.HEAD, new Item.Properties().group(ItemGroup.COMBAT)) {
+			@Override
+			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
+				return "sugarsnap:textures/models/armor/jadeamor_layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
+			}
+		}.setRegistryName("jade_armor_helmet"));
+		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.CHEST, new Item.Properties().group(ItemGroup.COMBAT)) {
+			@Override
+			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
+				return "sugarsnap:textures/models/armor/jadeamor_layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
+			}
+		}.setRegistryName("jade_armor_chestplate"));
+		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.LEGS, new Item.Properties().group(ItemGroup.COMBAT)) {
+			@Override
+			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
+				return "sugarsnap:textures/models/armor/jadeamor_layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
+			}
+		}.setRegistryName("jade_armor_leggings"));
+		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.FEET, new Item.Properties().group(ItemGroup.COMBAT)) {
+			@Override
+			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
+				return "sugarsnap:textures/models/armor/jadeamor_layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
+			}
+		}.setRegistryName("jade_armor_boots"));
+	}
+
+}
