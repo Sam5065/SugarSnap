dfc6ac2b17f25f91dc4b6fdca56a536e66fcdf0a
Sam5065 <sam.hilland1@icloud.com>
2024-05-12 19:44:59 +0100

    Changed armour

--- src/main/java/net/mcreator/sugarsnap/village/JadeTradeTrade.java
+++ src/main/java/net/mcreator/sugarsnap/village/JadeTradeTrade.java
@@ -12,6 +12,7 @@ import net.minecraft.entity.merchant.villager.VillagerTrades;
 import net.minecraft.entity.merchant.villager.VillagerProfession;
 
 import net.mcreator.sugarsnap.item.JadeitemItem;
+import net.mcreator.sugarsnap.item.JadeArmorItem;
 
 import java.util.List;
 
@@ -24,6 +25,16 @@ public class JadeTradeTrade {
 		Int2ObjectMap<List<VillagerTrades.ITrade>> trades = event.getTrades();
 		if (event.getType() == VillagerProfession.CLERIC) {
 			trades.get(5).add(new BasicTrade(new ItemStack(Items.GOLD_INGOT, (int) (10)), new ItemStack(JadeitemItem.block), 10, 5, 0.05f));
+		}
+		if (event.getType() == VillagerProfession.ARMORER) {
+			trades.get(5).add(new BasicTrade(new ItemStack(Items.EMERALD, (int) (10)), new ItemStack(JadeitemItem.block, (int) (8)),
+					new ItemStack(JadeArmorItem.body), 4, 5, 0.05f));
+			trades.get(3).add(new BasicTrade(new ItemStack(Items.EMERALD, (int) (8)), new ItemStack(JadeitemItem.block, (int) (7)),
+					new ItemStack(JadeArmorItem.legs), 4, 5, 0.05f));
+			trades.get(2).add(new BasicTrade(new ItemStack(Items.EMERALD, (int) (6)), new ItemStack(JadeitemItem.block, (int) (4)),
+					new ItemStack(JadeArmorItem.boots), 4, 5, 0.05f));
+			trades.get(4).add(new BasicTrade(new ItemStack(Items.EMERALD, (int) (7)), new ItemStack(JadeitemItem.block, (int) (5)),
+					new ItemStack(JadeArmorItem.helmet), 3, 5, 0.05f));
 		}
 	}
 }
