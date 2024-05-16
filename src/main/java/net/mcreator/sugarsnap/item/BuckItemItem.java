4cede92e221741d71171d0ed15ddfca8e7a1a77e
Sam5065 <sam.hilland1@icloud.com>
2024-05-16 21:05:33 +0100

    Added buckle item & sadle recipe

---
+++ src/main/java/net/mcreator/sugarsnap/item/BuckItemItem.java
@@ -0,0 +1,55 @@
+
+package net.mcreator.sugarsnap.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.UseAction;
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.sugarsnap.SugarsnapModElements;
+
+@SugarsnapModElements.ModElement.Tag
+public class BuckItemItem extends SugarsnapModElements.ModElement {
+	@ObjectHolder("sugarsnap:buck_item")
+	public static final Item block = null;
+
+	public BuckItemItem(SugarsnapModElements instance) {
+		super(instance, 34);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).rarity(Rarity.COMMON));
+			setRegistryName("buck_item");
+		}
+
+		@Override
+		public UseAction getUseAction(ItemStack itemstack) {
+			return UseAction.EAT;
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
